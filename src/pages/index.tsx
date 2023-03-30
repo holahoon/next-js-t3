25fd3aeb4bc1792a4f8c2056b15c6d02cb3ad708
David Kim <hola.hoon@gmail.com>
2023-03-31 08:42:10 +0900

    feat: image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,66 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
 
 import { api } from "~/utils/api";
+import type { RouterOutputs } from "~/utils/api";
+
+const CreatePostWizard = () => {
+  const { user } = useUser();
+  if (!user) return null;
+  return (
+    <div className="flex w-full gap-3">
+      <Image
+        className="h-14 w-14 rounded-full"
+        src={user.profileImageUrl}
+        alt="Profile image"
+        width={56}
+        height={56}
+      />
+      <input
+        placeholder="Type some emojis!"
+        className="grow rounded-full bg-transparent outline-none"
+      />
+    </div>
+  );
+};
+
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+const PostView = (props: PostWithUser) => {
+  const { post, author } = props;
+  return (
+    <div className="flex gap-3 border-b border-slate-400 p-4">
+      <Image
+        src={author.profileImageUrl}
+        alt="Author profile image"
+        className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
+      />
+      <div className="flex flex-col">
+        <div className="flex gap-1 font-bold text-slate-400">
+          <span>@{author.username}</span> .{" "}
+          <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
+        </div>
+        <span>{post.content}</span>
+      </div>
+    </div>
+  );
+};
 
 const Home: NextPage = () => {
   const user = useUser();
 
-  const { data } = api.posts.getAll.useQuery();
-  console.log(data);
+  const { data, isLoading } = api.posts.getAll.useQuery();
+
+  if (!data || isLoading) return <div>Loading...</div>;
+
+  if (!data) return <div>Something went wrong</div>;
 
   return (
     <>
@@ -18,14 +70,23 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div>{user.isSignedIn ? <SignOutButton /> : <SignOutButton />}</div>
-        {/* <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" /> */}
+      <main className="flex h-screen justify-center">
+        <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
+          <div className="flex border-b border-slate-400 p-4">
+            {user.isSignedIn ? (
+              <div className="flex justify-center">
+                <SignOutButton />
+                {/* <CreatePostWizard /> */}
+              </div>
+            ) : null}
+            {!user.isSignedIn ? <SignInButton /> : null}
+          </div>
 
-        <div>
-          {data?.map((post) => (
-            <div key={post.id}>{post.content}</div>
-          ))}
+          <div className="flex flex-col">
+            {data?.map((fullPost) => (
+              <PostView key={fullPost.post.id} {...fullPost} />
+            ))}
+          </div>
         </div>
       </main>
     </>
